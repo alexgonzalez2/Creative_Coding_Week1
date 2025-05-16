c4dc57e963c04c621a99c62b7f76152ab94e413c
alexgonzalez2 <alexmgonzalez22@gmail.com>
2025-05-16 18:05:56 -0400

    sketch start

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let mCanvas = createCanvas(windowWidth, windowHeight);
+  mCanvas.parent("main");
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
